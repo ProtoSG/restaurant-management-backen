a383c6398905f8c7713e2542c6f410bd60f0a917
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-09-28 13:16:37 -0500

    feat: init project

---
+++ src/test/java/com/restaurant_management/restaurant_management_backend/RestaurantManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant_management.restaurant_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
