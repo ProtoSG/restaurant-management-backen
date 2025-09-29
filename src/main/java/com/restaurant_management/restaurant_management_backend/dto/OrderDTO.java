1a7e465307c01b08fbfb18837767999950898b1a
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-09-29 16:50:26 -0500

    feat(order): add OrderDTO

---
+++ src/main/java/com/restaurant_management/restaurant_management_backend/dto/OrderDTO.java
@@ -0,0 +1,26 @@
+package com.restaurant_management.restaurant_management_backend.dto;
+
+import java.math.BigDecimal;
+
+import com.restaurant_management.restaurant_management_backend.enums.OrderStatus;
+import com.restaurant_management.restaurant_management_backend.enums.OrderType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Builder
+public class OrderDTO {
+
+  private Long id;
+  private Long tableId;
+  private OrderType type;
+  private OrderStatus status;
+  private BigDecimal total;
+
+}
