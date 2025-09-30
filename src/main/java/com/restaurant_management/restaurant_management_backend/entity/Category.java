d582c26eb3f1acad9ff6e783528b4ea88072bca8
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-09-29 22:57:53 -0500

    feat(category): add one to many relation with product

--- src/main/java/com/restaurant_management/restaurant_management_backend/entity/Category.java
+++ src/main/java/com/restaurant_management/restaurant_management_backend/entity/Category.java
@@ -1,10 +1,15 @@
 package com.restaurant_management.restaurant_management_backend.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,5 +32,9 @@ public class Category {
 
   @Column(name = "name")
   private String name;
+
+  @Builder.Default
+  @OneToMany(mappedBy = "category" ,fetch = FetchType.LAZY, orphanRemoval = true)
+  private List<Product> products = new ArrayList<>();
 
 }
