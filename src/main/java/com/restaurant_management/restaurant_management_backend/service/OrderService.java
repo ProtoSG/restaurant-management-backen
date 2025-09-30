8ca60317e6643b6eb9e688d09e12d7e424cf54ec
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-09-29 23:03:57 -0500

    feat(order): update service for order

--- src/main/java/com/restaurant_management/restaurant_management_backend/service/OrderService.java
+++ src/main/java/com/restaurant_management/restaurant_management_backend/service/OrderService.java
@@ -3,12 +3,19 @@ package com.restaurant_management.restaurant_management_backend.service;
 import java.util.List;
 
 import com.restaurant_management.restaurant_management_backend.dto.OrderDTO;
-import com.restaurant_management.restaurant_management_backend.dto.OrderItemDTO;
+import com.restaurant_management.restaurant_management_backend.dto.OrderItemsDTO;
+import com.restaurant_management.restaurant_management_backend.dto.OrderTypeDTO;
 
 public interface OrderService {
   
   public OrderDTO save(OrderDTO orderDTO);
+  public OrderDTO findById(Long id);
+  public List<OrderDTO> findAll();
+  public void delete(Long id);
 
-  public void addProducts(Long orderId, List<OrderItemDTO> orderItems);
-  
+  public OrderDTO changeTable(Long orderId, Long tableId);
+  public OrderDTO changeType(Long id, OrderTypeDTO type);
+  public OrderDTO cancelOrder(Long id);
+  public void addProducts(Long orderId, OrderItemsDTO orderItems);
+
 }
--- src/main/java/com/restaurant_management/restaurant_management_backend/service/impl/OrderServiceImpl.java
+++ src/main/java/com/restaurant_management/restaurant_management_backend/service/impl/OrderServiceImpl.java
@@ -1,6 +1,5 @@
 package com.restaurant_management.restaurant_management_backend.service.impl;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -8,10 +7,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.restaurant_management.restaurant_management_backend.dto.OrderDTO;
 import com.restaurant_management.restaurant_management_backend.dto.OrderItemDTO;
+import com.restaurant_management.restaurant_management_backend.dto.OrderItemsDTO;
+import com.restaurant_management.restaurant_management_backend.dto.OrderTypeDTO;
 import com.restaurant_management.restaurant_management_backend.entity.Order;
 import com.restaurant_management.restaurant_management_backend.entity.OrderItem;
 import com.restaurant_management.restaurant_management_backend.entity.Product;
 import com.restaurant_management.restaurant_management_backend.entity.Table;
+import com.restaurant_management.restaurant_management_backend.enums.OrderStatus;
+import com.restaurant_management.restaurant_management_backend.enums.OrderType;
 import com.restaurant_management.restaurant_management_backend.exceptions.ResourceNotFoundException;
 import com.restaurant_management.restaurant_management_backend.mapper.OrderMapper;
 import com.restaurant_management.restaurant_management_backend.repository.OrderItemRepository;
@@ -35,27 +38,86 @@ public class OrderServiceImpl implements OrderService {
 
   @Transactional
   public OrderDTO save(OrderDTO orderDTO) {
-    
-    Order newOrder = orderMapper.toEntity(orderDTO);
-    
+    Order newOrder = Order.builder().build();
+
     Table table = tableRepository.findById(orderDTO.getTableId())
         .orElseThrow(() -> new ResourceNotFoundException("Mesa no encontrada"));
 
     newOrder.assignToTable(table);
-    table.occupy();
 
-    Order savedOrder = orderRepository.save(newOrder);
-    tableRepository.save(table);
+    return orderMapper.toDto(orderRepository.save(newOrder));
+  }
 
-    return orderMapper.toDto(savedOrder);
+  @Transactional(readOnly = true)
+  public OrderDTO findById(Long id) {
+    Order order = orderRepository.findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException("Pedido no encontrado"));
+
+    return orderMapper.toDto(order);
+  }
+
+  @Transactional(readOnly = true)
+  public List<OrderDTO> findAll() {
+    List<Order> orders = orderRepository.findAll();
+
+    return orders.stream()
+      .map(orderMapper::toDto)
+      .toList();
   }
 
   @Transactional
-  public void addProducts(Long orderId, List<OrderItemDTO> orderItemsDtos) {
+  public void delete(Long id) {
+    Order order = orderRepository.findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException("Pedido no encontrado"));
+
+    Table table = order.getTable();
+    table.free();
+
+    orderRepository.delete(order);
+  }
+
+  @Transactional
+  public OrderDTO changeTable(Long orderId, Long tableId) {
     Order order = orderRepository.findById(orderId)
       .orElseThrow(() -> new ResourceNotFoundException("Pedido no encontrado"));
 
-    for (OrderItemDTO orderItemDto : orderItemsDtos) {
+    Table oldTable = order.getTable();
+    oldTable.free();
+
+    Table table = tableRepository.findById(tableId)
+      .orElseThrow(() -> new ResourceNotFoundException("Mesa no encontrada"));
+
+    order.assignToTable(table);
+
+    return orderMapper.toDto(orderRepository.save(order));
+  }
+
+  @Transactional
+  public OrderDTO changeType(Long id, OrderTypeDTO orderTypeDTO) {
+    Order order = orderRepository.findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException("Pedido no encontrado"));
+
+    order.setType(orderTypeDTO.getType());
+
+    return orderMapper.toDto(orderRepository.save(order));
+  }
+
+  @Transactional
+  public OrderDTO cancelOrder(Long id) {
+    Order order = orderRepository.findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException("Pedido no encontrado"));
+
+    order.setStatus(OrderStatus.CANCELLED);
+
+    return orderMapper.toDto(orderRepository.save(order));
+  }
+
+  @Transactional
+  public void addProducts(Long orderId, OrderItemsDTO orderItemsDTO) {
+    Order order = orderRepository.findById(orderId)
+      .orElseThrow(() -> new ResourceNotFoundException("Pedido no encontrado"));
+
+    for (OrderItemDTO orderItemDto : orderItemsDTO.getItems()) {
       Product product = productRepository.findById(orderItemDto.getProductId())
         .orElseThrow(() -> new ResourceNotFoundException("Producto no encontrado"));
 
@@ -67,6 +129,8 @@ public class OrderServiceImpl implements OrderService {
 
       order.addItem(orderItem);
     }
+
+    orderRepository.save(order);
   }
 
 }
