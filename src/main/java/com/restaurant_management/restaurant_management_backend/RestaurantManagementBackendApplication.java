8f96c8c0c39b8bec1ee7c5ed722dfe64a62108ee
ProtoSG <diegoalbertosalzar177@gmail.com>
2026-02-09 11:49:14 -0500

    feat: update config project

--- src/main/java/com/restaurant_management/restaurant_management_backend/RestaurantManagementBackendApplication.java
+++ src/main/java/com/restaurant_management/restaurant_management_backend/RestaurantManagementBackendApplication.java
@@ -3,10 +3,18 @@ package com.restaurant_management.restaurant_management_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class RestaurantManagementBackendApplication {
 
 	public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+
+    dotenv.entries().forEach(entry -> {
+      System.setProperty(entry.getKey(), entry.getValue());
+    });
+
 		SpringApplication.run(RestaurantManagementBackendApplication.class, args);
 	}
 
--- src/main/java/com/restaurant_management/restaurant_management_backend/security/SecurityConfig.java
+++ src/main/java/com/restaurant_management/restaurant_management_backend/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.restaurant_management.restaurant_management_backend.config;
+package com.restaurant_management.restaurant_management_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.restaurant_management.restaurant_management_backend.enums.RoleName;
 import com.restaurant_management.restaurant_management_backend.exceptions.UnauthorizedException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,52 +25,46 @@ public class SecurityConfig {
 
   private final JwtAuthFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
-  private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
   @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
       .cors(Customizer.withDefaults())
       .csrf(csrf -> csrf.disable())
       .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/auth/**", "/health")
-          .permitAll()
-          .anyRequest()
-          .authenticated()
+        .requestMatchers("/auth/**", "/health").permitAll()
+        .anyRequest().authenticated()
       )
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authenticationProvider(authenticationProvider)
       .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-      .exceptionHandling(exception -> exception
-        .authenticationEntryPoint(customAuthenticationEntryPoint)
-      )
-      .logout(logout -> 
-          logout.logoutUrl("/auth/logout")
-          .addLogoutHandler((request, response, authentication) -> {
-            final var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-            logout(authHeader, request, response);
-          })
-          .logoutSuccessHandler((request, response, authentication) ->
-            SecurityContextHolder.clearContext())
-          );
+      .logout(logout ->
+        logout.logoutUrl("/auth/logout")
+        .permitAll()
+        .addLogoutHandler((request, response, authentication) -> {
+          final var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+          logout(authHeader, request, response);
+        })
+        .logoutSuccessHandler((request, response, authentication) -> 
+          SecurityContextHolder.clearContext()
+        )
+      );
 
     return http.build();
   }
 
   private void logout(final String authHeader, HttpServletRequest request, HttpServletResponse response) {
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-      throw new UnauthorizedException("Invalid token");
+      throw new UnauthorizedException("token invalid");
     }
-
     ResponseCookie cleared = ResponseCookie.from("refresh_token", "")
       .httpOnly(true)
-      .secure(true)
-      .sameSite("None")
-      .path("/auth/refresh")
+      .secure(false) //NOTE: cambiar en produccion(True)
+      .sameSite("Lax") //NOTE: cambiar en produccion(None)
+      .path("/api/auth")
       .maxAge(0)
       .build();
 
     response.addHeader(HttpHeaders.SET_COOKIE, cleared.toString());
   }
-
 }
